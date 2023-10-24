4e379e8905d43a36aa37ee08e71fe9e50b65b5d7
Alfred-Onuada <aonuada5@gmail.com>
2023-10-24 17:31:46 +0100

    tiny fix

--- js/autocomplete.js
+++ js/autocomplete.js
@@ -111,6 +111,7 @@ document.addEventListener('DOMContentLoaded', initializeAutoComplete());
 
 
 function focusInputDiv() {
+  document.getElementById('myTabContent').scrollIntoView();
   document.getElementById('postcode-search').focus()
   document.getElementById('myTabContent').classList.add("pulse")
 
